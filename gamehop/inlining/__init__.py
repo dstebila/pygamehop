6781b0bfe5e2ef8867b1e0770270deff55141a00
Douglas Stebila <dstebila@uwaterloo.ca>
2021-02-03 20:05:59 -0500

    Inlining functions

--- gamehop/inlining/__init__.py
+++ gamehop/inlining/__init__.py
@@ -1,4 +1,5 @@
 import ast
+import copy
 import inspect
 import types
 from typing import Any, Callable, Union
@@ -41,3 +42,61 @@ def inline_argument(f: Union[Callable, str], arg: str, val: Union[bool, float, i
             break
     # return the resulting function
     return ast.unparse(ast.fix_missing_locations(newfdef))
+
+def inline_function(inlinee: Union[Callable, str], inlinand: Union[Callable, str]) -> str:
+    """Returns a string representing the second function inlined into the first.
+
+    Only works on calls to the function in an Assign statement."""
+    # get the function definitions
+    inlinee_def = internal.get_function_def(inlinee)
+    inlinand_def = copy.deepcopy(internal.get_function_def(inlinand))
+    # check that the inlinand has only a single return as the last statement
+    class ContainsReturn(ast.NodeVisitor):
+        def visit_Return(self, node):
+            raise NotImplementedError("Function to be inlined has a return statement somewhere other than the last line")
+    for i in range(len(inlinand_def.body) - 1):
+        ContainsReturn().visit(inlinand_def.body[i])
+    if not isinstance(inlinand_def.body[-1], ast.Return): raise NotImplementedError("Function to be inlined does not end with a return statement")
+    # define the inliner which will do the replacements
+    class Inliner(ast.NodeTransformer):
+        def __init__(self, inlinee_def, inlinand_def):
+            self.inlinee_def = inlinee_def
+            self.inlinand_def = inlinand_def
+            self.variables_in_inlinee = internal.find_all_variables(inlinee_def)
+            self.variables_in_inlinand = internal.find_all_variables(inlinand_def)
+            self.replacement_count = 0
+        def visit_Assign(self, stmt):
+            if isinstance(stmt.value, ast.Call) and stmt.value.func.id == self.inlinand_def.name:
+                self.replacement_count += 1
+                # prefix all variables in inlinand
+                mappings = dict()
+                for var in self.variables_in_inlinand: 
+                    mappings[var] = 'v_{:s}_{:d}_{:s}'.format(self.inlinand_def.name, self.replacement_count, var)
+                newinlinand_def = internal.rename_variables(self.inlinand_def, mappings)
+                # map the parameters onto the arguments
+                assert len(stmt.value.args) == len(newinlinand_def.args.args)
+                for i in range(len(stmt.value.args)):
+                    arg = stmt.value.args[i]
+                    if isinstance(arg, ast.Name):
+                        mapping = {newinlinand_def.args.args[i].arg: stmt.value.args[i].id}
+                        newinlinand_def = internal.rename_variables(newinlinand_def, mapping, error_if_exists = False)
+                    elif isinstance(arg, ast.Constant):
+                        newinlinand_def = internal.get_function_def(inline_argument(newinlinand_def, newinlinand_def.args.args[i].arg, stmt.value.args[i].value))
+                    else: raise NotImplementedError("Don't know how to inline calls whose arguments are of type {:s}".format(type(arg).__name__))
+                # turn the final return statement into an assignment
+                assert isinstance(newinlinand_def.body[-1], ast.Return)
+                newret = ast.Assign(targets=stmt.targets, value=newinlinand_def.body[-1].value)
+                newinlinand_def.body[-1] = newret
+                # output is the inlined function body
+                return newinlinand_def.body
+            else: return stmt
+    # run the inliner
+    newinlinee_def = Inliner(inlinee_def, inlinand_def).visit(inlinee_def)
+    # if there's still a call to our function somewhere, it must have been somewhere other than on a bare Assign line; raise an error
+    class ContainsCall(ast.NodeVisitor):
+        def __init__(self, funcname):
+            self.funcname = funcname
+        def visit_Call(self, node):
+            if node.func.id == self.funcname: raise NotImplementedError("Can't inline functions that are called on anything other than as a solitary assignment line, e.g., y = f(x)")
+    ContainsCall(inlinand_def.name).visit(newinlinee_def)
+    return ast.unparse(ast.fix_missing_locations(newinlinee_def))
---
+++ tests/gamehop/inlining/test_inline_function.py
@@ -0,0 +1,81 @@
+import ast
+import inspect
+import unittest
+
+import gamehop.inlining
+
+def inlinand(a, b):
+    c = a + b
+    return c
+def inlinand_compound_return(a, b):
+    c = a + b
+    return c + 7
+def inlinand_no_return_at_end(a, b):
+    c = a + b
+def inlinand_multiple_returns(a, b):
+    if a: return b
+    elif not(a): return a
+    return a + b
+
+def f_variable(x):
+    y = inlinand(x, x)
+def f_variable_expected_result(x):
+    v_inlinand_1_c = x + x
+    y = v_inlinand_1_c
+def f_constant(x):
+    y = inlinand(x, 3)
+def f_constant_expected_result(x):
+    v_inlinand_1_c = x + 3
+    y = v_inlinand_1_c
+def f_several_calls(x):
+    w = inlinand(x, x)
+    y = inlinand(x, x)
+def f_several_calls_expected_result(x):
+    v_inlinand_1_c = x + x
+    w = v_inlinand_1_c
+    v_inlinand_2_c = x + x
+    y = v_inlinand_2_c
+def f_compound_return(x):
+    y = inlinand_compound_return(x, 3)
+def f_compound_return_expected_result(x):
+    v_inlinand_compound_return_1_c = x + 3
+    y = v_inlinand_compound_return_1_c + 7
+def f_inlinand_nested(x):
+    y = inlinand(inlinand(x, 3), 4)
+def f_inlinand_in_operation(x):
+    y = inlinand(x, 3) + 4
+
+def expected_result(f):
+    s = inspect.getsource(f)
+    s = s.replace('_expected_result', '')
+    return ast.unparse(ast.parse(s))
+
+class TestInlineFunction(unittest.TestCase):
+    def test_variable(self):
+        self.assertEqual(
+            gamehop.inlining.inline_function(f_variable, inlinand),
+            expected_result(f_variable_expected_result))
+    def test_constant(self):
+        self.assertEqual(
+            gamehop.inlining.inline_function(f_constant, inlinand),
+            expected_result(f_constant_expected_result))
+    def test_several_calls(self):
+        self.assertEqual(
+            gamehop.inlining.inline_function(f_several_calls, inlinand),
+            expected_result(f_several_calls_expected_result))
+    def test_compound_return(self):
+        self.assertEqual(
+            gamehop.inlining.inline_function(f_compound_return, inlinand_compound_return),
+            expected_result(f_compound_return_expected_result))
+    def test_inlinand_nested(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.inlining.inline_function(f_inlinand_nested, inlinand)
+    def test_inlinand_in_operation(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.inlining.inline_function(f_inlinand_in_operation, inlinand)
+    def test_no_return_at_end(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.inlining.inline_function(f_variable, inlinand_no_return_at_end)
+    def test_multiple_returns(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.inlining.inline_function(f_variable, inlinand_multiple_returns)
