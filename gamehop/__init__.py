04f327e6913c3745fbf276c9bdd13cde93f653c0
Matthew McKague <matthew.mckague@gmail.com>
2021-02-08 11:03:54 +1000

    Fixed type problems

--- gamehop/__init__.py
+++ gamehop/__init__.py
@@ -1,24 +1,27 @@
 __all__ = ['primitives']
 
-from typing import Any, Callable, List, Set, Union
-import gamehop.inlining
-import gamehop.verification
+from typing import Any, Callable, List, Set, Union, cast
+from gamehop import inlining
+from gamehop import verification
 import ast
 import inspect
 
 def inline(inlinee: Union[Callable, str, ast.FunctionDef], inlinand: Union[Callable, str, ast.FunctionDef, object, ast.ClassDef], arg = '', inline_init = True, search_function_name=None, dest_function_name=None, self_prefix="") -> str:
     inlined = ''
     if type(inlinand) == str:
-        if str.startswith(inlinand, 'def'):
-            inlined = inlining.inline_function(inlinee, inlinand, search_function_name=search_function_name, dest_function_name= dest_function_name, self_prefix=self_prefix)
-        elif str.startswith(inlinand, 'class'):
-            inlined = inlining.inline_class(inlinee, arg, inlinand, inline_init = inline_init)
+        inlinand_str = cast(str, inlinand)
+        if str.startswith(inlinand_str, 'def'):
+            inlined = inlining.inline_function(inlinee, inlinand_str, search_function_name=search_function_name, dest_function_name= dest_function_name, self_prefix=self_prefix)
+        elif str.startswith(inlinand_str, 'class'):
+            inlined = inlining.inline_class(inlinee, arg, inlinand_str, inline_init = inline_init)
         else:
-            raise error('String not function or class definition')
+            raise Exception('String not function or class definition')
     elif type(inlinand) == Callable or type(inlinand) == ast.FunctionDef:
-        inlined = inlining.inline_function(inlinee, inlinand, search_function_name=search_function_name, dest_function_name= dest_function_name, self_prefix=self_prefix)
+        inlinand_f = cast(Union[Callable, ast.FunctionDef], inlinand)
+        inlined = inlining.inline_function(inlinee, inlinand_f, search_function_name=search_function_name, dest_function_name= dest_function_name, self_prefix=self_prefix)
     elif type(inlinand) == ast.ClassDef or type(inlinand) == object or inspect.isclass(inlinand):
-        inlined = inlining.inline_class(inlinee, arg, inlinand, inline_init = inline_init)
+        inlinand_class = cast(Union[object, ast.ClassDef], inlinand)
+        inlined = inlining.inline_class(inlinee, arg, inlinand_class, inline_init = inline_init)
 
     r = verification.canonicalize_function(inlined)
     return r
@@ -30,7 +33,7 @@ def assertEqual(a: str, b: str, debugging=False):
         print("Strings differ")
         stringDiff(a,b)
     else:
-        prints("Strings identical")
+        print("Strings identical")
 
 def advantage(a: str, b: str, scheme, experiment):
     pass
