8abdccb0e7ac0446276a48aec99a888bd1390a43
Douglas Stebila <dstebila@uwaterloo.ca>
2021-03-30 15:33:30 -0400

    Refactor formatting

--- gamehop/proofs.py
+++ gamehop/proofs.py
@@ -1,5 +1,7 @@
 from abc import ABC
+import inspect
 from typing import List, Type
+import typing
 
 from .primitives import Crypto
 from . import inlining
@@ -11,6 +13,14 @@ class DistinguishingExperiment(Experiment):
     def main1(self, scheme, adversary) -> Crypto.Bit: pass
 class GuessingExperiment(Experiment):
     def main(self, scheme, adversary, bit: Crypto.Bit) -> Crypto.Bit: pass
+
+def fqn(o):
+    if inspect.isclass(o):
+        if o.__module__ == '__main__': return o.__name__
+        else: return "{:s}.{:s}".format(o.__module__, o.__name__)
+    elif isinstance(o, typing._GenericAlias):
+        return typing.get_origin(o).__name__
+    else: raise ValueError("Don't know how to find the name of objects of type {:s}".format(str(type(o))))
 
 class Proof():
     def __init__(self, experiment: Type[Experiment], scheme, adversary):
@@ -30,10 +40,7 @@ class Proof():
         if issubclass(self.experiment, DistinguishingExperiment):
             startGame = inlining.inline_class(self.experiment.main0, 'scheme', self.scheme)
             print("==== STARTING GAME ====");
-            startGameDescription = "experiment {:s}.{:s}.main0 inlined with scheme {:s}.{:s}".format(
-                self.experiment.__module__, self.experiment.__name__,
-                self.scheme.__module__, self.scheme.__name__
-            )
+            startGameDescription = "experiment {:s}.main0 inlined with scheme {:s}".format(fqn(self.experiment), fqn(self.scheme))
             print("---- {:s} ----".format(startGameDescription))
             if print_hops:
                 print(startGame)
@@ -42,24 +49,15 @@ class Proof():
                     print(verification.canonicalize_function(startGame))
             previousGame = startGame
             previousGameDescription = startGameDescription
-            for stepNum in range(len(self.proofSteps)):
-                step = self.proofSteps[stepNum]
+            for stepNum, step in enumerate(self.proofSteps):
                 if step['type'] == 'distinguishingProofStep':
                     leftInlining = inlining.inline_class(step['experiment'].main0, 'adversary', step['reduction'])
                     rightInlining = inlining.inline_class(step['experiment'].main1, 'adversary', step['reduction'])
                     s1 = verification.canonicalize_function(previousGame)
                     s2 = verification.canonicalize_function(leftInlining)
                     if step['reverseDirection']: (rightInlining, leftInlining) = (leftInlining, rightInlining)
-                    leftInliningDescription = "reduction {:s} inlined with {:s}.{:s}.main{:d}".format(
-                        step['reduction'].__name__,
-                        step['experiment'].__module__, step['experiment'].__name__,
-                        0 if not step['reverseDirection'] else 1
-                    )
-                    rightInliningDescription = "reduction {:s} inlined with {:s}.{:s}.main{:d}".format(
-                        step['reduction'].__name__,
-                        step['experiment'].__module__, step['experiment'].__name__,
-                        1 if not step['reverseDirection'] else 0
-                    )
+                    leftInliningDescription = "reduction {:s} inlined with {:s}.main{:d}".format(fqn(step['reduction']), fqn(step['experiment']), 0 if not step['reverseDirection'] else 1)
+                    rightInliningDescription = "reduction {:s} inlined with {:s}.main{:d}".format(fqn(step['reduction']), fqn(step['experiment']), 1 if not step['reverseDirection'] else 0)
                     print("==== GAME HOP from {:d} to {:d} (distinguishing proof step) ====".format(stepNum, stepNum + 1))
                     print("---- {:s} ----".format(leftInliningDescription))
                     if print_hops:
@@ -81,17 +79,11 @@ class Proof():
                             print(verification.canonicalize_function(rightInlining))
                 else: raise TypeError("Unsupported proof step type: {:s}.".format(str(step['type'])))
             endGame = inlining.inline_class(self.experiment.main1, 'scheme', self.scheme)
-            endGameDescription = "experiment {:s}.{:s}.main1 inlined with scheme {:s}.{:s}".format(
-                self.experiment.__module__, self.experiment.__name__,
-                self.scheme.__module__, self.scheme.__name__
-            )
+            endGameDescription = "experiment {:s}.main1 inlined with scheme {:s}".format(fqn(self.experiment), fqn(self.scheme))
             s1 = verification.canonicalize_function(previousGame)
             s2 = verification.canonicalize_function(endGame)
             print("==== ENDING GAME ====");
-            print("---- experiment {:s}.{:s}.main1 inlined with scheme {:s}.{:s}".format(
-                self.experiment.__module__, self.experiment.__name__,
-                self.scheme.__module__, self.scheme.__name__
-            ))
+            print("---- {:s} ----".format(endGameDescription))
             if print_hops:
                 print(endGame)
                 if print_canonicalizations:
