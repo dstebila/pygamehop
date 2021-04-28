a3a50f41995ce45d32502d5feadafd47b9871ed1
Douglas Stebila <dstebila@uwaterloo.ca>
2021-04-28 17:34:27 -0400

    More robust parsing for canonicalize_function

--- gamehop/verification/__init__.py
+++ gamehop/verification/__init__.py
@@ -27,14 +27,10 @@ def canonicalize_function(f: Union[Callable, str]) -> str:
     - variable names are 'v0', 'v1', ...
     - lines are reordered based on variable dependencies"""
     # parse the function
-    if isinstance(f, FunctionType): t = ast.parse(inspect.getsource(f))
-    elif isinstance(f, str): t = ast.parse(f)
-    else: raise TypeError()
-    # make sure it is a single function
-    functionDef = t.body[0]
+    functionDef = internal.get_function_def(f)
     assert isinstance(functionDef, ast.FunctionDef)
     str_previous = ""
-    str_current = ast.unparse(ast.fix_missing_locations(t))
+    str_current = ast.unparse(ast.fix_missing_locations(functionDef))
     while str_previous != str_current:
         str_previous = str_current
         expand.call_arguments(functionDef)
@@ -57,5 +53,5 @@ def canonicalize_function(f: Union[Callable, str]) -> str:
         debug_helper(functionDef, "canonicalization.canonicalize_argument_order")
         canonicalization.canonicalize_variable_names(functionDef)
         debug_helper(functionDef, "canonicalization.canonicalize_variable_names")
-        str_current = ast.unparse(ast.fix_missing_locations(t))
+        str_current = ast.unparse(ast.fix_missing_locations(functionDef))
     return str_current
