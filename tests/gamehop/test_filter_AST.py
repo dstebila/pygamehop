26baf86b17e0b25582671b6ed14d3a0c01dcbd0c
Matthew McKague <matthew.mckague@gmail.com>
2021-07-28 13:03:06 +1000

    unit tests for ast filter finished.

--- tests/gamehop/test_filter_AST.py
+++ tests/gamehop/test_filter_AST.py
@@ -26,10 +26,10 @@ def f_Delete():
 # AnnAssign to tested
 
 def f_For():
-    for a in range(1,2): pass
+    for a in S: pass
 
 # this one is a string because otherwise we would need to put it in an async function, which is another case.
-async_for_str = "async for i in range(1,2): pass"
+async_for_str = "async for i in S: pass"
 
 def f_With():
     b = 1
@@ -52,6 +52,73 @@ def f_ImportFrom():
     from . import blarg
 def f_Global():
     global x
+def f_Nonlocal():
+    x = 1
+    def myfunc():
+        nonlocal x
+
+# these are in strings because they usually only come in for or while loops, which are other cases
+pass_str = "pass"
+break_str = "break"
+continue_str = "continue"
+
+def f_NamedExpr():
+    (x := 1)
+def f_Dict():
+    x = {}
+def f_Set():
+    x = { 1 }
+def f_List():
+    x = []
+def f_ListComp():
+    x = [ i for i in S ]
+def f_SetComp():
+    x = { i for i in S }
+def f_DictComp():
+    x = { i:i for i in S }
+def f_GeneratorExp():
+    x = ( i for i in S )
+
+await_str = "await myfunc()"
+
+def f_Yield():
+    yield 1
+def f_YieldFrom():
+    yield from myfunc()
+def f_FormattedValue():
+    x = f"{var}"
+def f_JoinedStr():
+    x = f"blah"
+
+starred_str = "*b = it"
+
+def f_Slice():
+    x = mylist[1:2]
+
+# These tests are for nodes that are supported, but certain attributes are forbidden
+
+def f_decorators():
+    @mydecorator
+    def myfunc(): pass
+def f_posonlyargs():
+    def myfunc(a, /, b): pass
+def f_kwonlyargs():
+    def myfunc(a, *, b): pass
+def f_starargs():
+    def myfunc(*args): pass
+def f_kwstarargs():
+    def myfunc(**kwargs): pass
+def f_defaultargs():
+    def myfunc(a=1): pass
+def f_kwargscall():
+    x = myfunc(a = 1)
+
+# type_ignore?
+
+# The following have no tests because they only appear in other nodes that are already forbidden, so we can't create isolated cases for them:
+#   comprehension, excepthandler, keyword, alias, withitem,
+# Similar for these attributes:
+#   kw_defaults
 
 
 class TestFilterAST(unittest.TestCase):
@@ -97,3 +164,81 @@ class TestFilterAST(unittest.TestCase):
     def test_Global(self):
         with self.assertRaises(NotImplementedError):
             gamehop.filterast.filter_AST(internal.get_function_def(f_Global))
+    def test_Nonlocal(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_Nonlocal))
+    def test_Pass(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(ast.parse(pass_str))
+    def test_Break(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(ast.parse(break_str))
+    def test_Continue(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(ast.parse(continue_str))
+    def test_NamedExpr(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_NamedExpr))
+    def test_Dict(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_Dict))
+    def test_Set(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_Set))
+    def test_List(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_List))
+    def test_ListComp(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_ListComp))
+    def test_SetComp(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_SetComp))
+    def test_DictComp(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_DictComp))
+    def test_GeneratorExp(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_GeneratorExp))
+    def test_Await(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(ast.parse(await_str))
+    def test_Yield(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_Yield))
+    def test_YieldFrom(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_YieldFrom))
+    def test_YieldFormattedValue(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_FormattedValue))
+    def test_JoinedStr(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_JoinedStr))
+    def test_Starred(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(ast.parse(starred_str))
+    def test_Slice(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_Slice))
+    def test_Decorator(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_decorators))
+    def test_Posonlyargs(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_posonlyargs))
+    def test_Kwonlyargs(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_kwonlyargs))
+    def test_Starargs(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_starargs))
+    def test_Kwstarargs(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_kwstarargs))
+    def test_defaultargs(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_defaultargs))
+    def test_kwargscall(self):
+        with self.assertRaises(NotImplementedError):
+            gamehop.filterast.filter_AST(internal.get_function_def(f_kwargscall))
