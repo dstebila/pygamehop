c23f5be9f517f339b9635fde485a26a6b78048a7
Douglas Stebila <dstebila@uwaterloo.ca>
2021-08-13 16:04:13 -0400

    Add rewriting steps to nestPKE_is_INDCPA proof

--- examples/nestedPKE/nestedPKE_is_INDCPA.py
+++ examples/nestedPKE/nestedPKE_is_INDCPA.py
@@ -42,7 +42,41 @@ print(proof1.advantage_bound())
 # term we're trying to bound, first in terms of PKE1 security
 proof2 = Proof(PKE.INDCPA, nestedPKE.Scheme, PKEINDCPA_adversary)
 
-# game hop:
+# gamehop: rewriting step
+# len(PKEScheme.Encrypt(pk, m0)) = len(PKEScheme.Encrypt(pk, m1)) assuming len(m0) == len(m1)
+def rewrite_left0(v0: PKEScheme, v1: PKEINDCPA_adversary, v2: PKEScheme) -> Crypto.Bit:
+    (v3, v4) = v0.KeyGen()
+    (v5, v6) = v2.KeyGen()
+    (v7, v8) = v1.challenge((v3, v5))
+    v9 = v0.Encrypt(v3, v7)
+    v10 = v2.Encrypt(v5, v9)
+    v11 = v1.guess(v10)
+    v12 = len(v7) == len(v8)
+    v13 = Crypto.Bit(0)
+    v14 = v0.Encrypt(v3, v8)
+    v15 = v11 if v12 else v13
+    v16 = True
+    v17 = Crypto.Bit(0)
+    v18 = v15 if v16 else v17
+    return v18
+def rewrite_right0(v0: PKEScheme, v1: PKEINDCPA_adversary, v2: PKEScheme) -> Crypto.Bit:
+    (v3, v4) = v0.KeyGen()
+    (v5, v6) = v2.KeyGen()
+    (v7, v8) = v1.challenge((v3, v5))
+    v9 = v0.Encrypt(v3, v7)
+    v10 = v2.Encrypt(v5, v9)
+    v11 = v1.guess(v10)
+    v12 = len(v7) == len(v8)
+    v13 = Crypto.Bit(0)
+    v14 = v0.Encrypt(v3, v8)
+    v15 = v11 if v12 else v13
+    v16 = len(v9) == len(v14)
+    v17 = Crypto.Bit(0)
+    v18 = v15 if v16 else v17
+    return v18
+proof2.addRewritingStep(rewrite_left0, rewrite_right0)
+
+# game hop: distinguishing step
 # encrypt m1 rather than m0; simulate PKE1's encryption
 # proven by constructing reduction from distinguishing the previous game and this game to distinguishing PKE.INDCPA (with b = 0) from PKE.INDCPA (with b = 1) for PKE2
 class R2(PKEINDCPA_adversary): # this is an adversary for PKE2
@@ -58,14 +92,50 @@ class R2(PKEINDCPA_adversary): # this is an adversary for PKE2
         (self.pk1, self.sk1)  = self.pke1.KeyGen()
         pk_double = (self.pk1, self.pk2)
         (m0, m1) = self.adversary.challenge(pk_double)
+        self.ok = len(m0) == len(m1)
         i0 = self.pke1.Encrypt(self.pk1, m0)
         i1 = self.pke1.Encrypt(self.pk1, m1)
         return (i0, i1)
     def guess(self, ct: PKE.Ciphertext) -> Crypto.Bit:
-        return self.adversary.guess(ct)
+        r = self.adversary.guess(ct)
+        return r if self.ok else Crypto.Bit(0)
 
 proof2.addDistinguishingProofStep(PKE.INDCPA, PKEScheme, R2)
 
-assert proof2.check(print_hops=False, print_canonicalizations=False)
+# gamehop: rewriting step
+# len(PKEScheme.Encrypt(pk, m0)) = len(PKEScheme.Encrypt(pk, m1)) assuming len(m0) == len(m1)
+def rewrite_left2(v0: PKEScheme, v1: PKEINDCPA_adversary, v2: PKEScheme) -> Crypto.Bit:
+    (v3, v4) = v0.KeyGen()
+    (v5, v6) = v2.KeyGen()
+    (v7, v8) = v1.challenge((v3, v5))
+    v9 = v0.Encrypt(v3, v8)
+    v10 = v2.Encrypt(v5, v9)
+    v11 = v1.guess(v10)
+    v12 = len(v7) == len(v8)
+    v13 = Crypto.Bit(0)
+    v14 = v0.Encrypt(v3, v7)
+    v15 = v11 if v12 else v13
+    v16 = len(v14) == len(v9)
+    v17 = Crypto.Bit(0)
+    v18 = v15 if v16 else v17
+    return v18
+def rewrite_right2(v0: PKEScheme, v1: PKEINDCPA_adversary, v2: PKEScheme) -> Crypto.Bit:
+    (v3, v4) = v0.KeyGen()
+    (v5, v6) = v2.KeyGen()
+    (v7, v8) = v1.challenge((v3, v5))
+    v9 = v0.Encrypt(v3, v8)
+    v10 = v2.Encrypt(v5, v9)
+    v11 = v1.guess(v10)
+    v12 = len(v7) == len(v8)
+    v13 = Crypto.Bit(0)
+    v14 = v0.Encrypt(v3, v7)
+    v15 = v11 if v12 else v13
+    v16 = True
+    v17 = Crypto.Bit(0)
+    v18 = v15 if v16 else v17
+    return v18
+proof2.addRewritingStep(rewrite_left2, rewrite_right2)
+
+assert proof2.check(print_hops=True, print_canonicalizations=True)
 print()
 print(proof2.advantage_bound())
