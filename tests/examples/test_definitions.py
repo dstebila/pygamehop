f1ffdc86132d94f22f0af9649494f328173c32b8
Douglas Stebila <dstebila@uwaterloo.ca>
2021-08-16 15:36:49 -0400

    Fix typo

--- tests/examples/test_definitions.py
+++ tests/examples/test_definitions.py
@@ -1,11 +1,11 @@
 import unittest
 
-from gamehop.primitives import kem
+from gamehop.primitives import KEM
 import gamehop.inlining
 from gamehop.verification import canonicalize_function
 
 class TestDefinitions(unittest.TestCase):
 
-    def old_test_KEM_INDCPA_definitions_equivalent(self): 
-        assert canonicalize_function(kem.INDCPAv2.get_left()) == canonicalize_function(kem.INDCPA.get_left())
-        assert canonicalize_function(kem.INDCPAv2.get_right()) == canonicalize_function(kem.INDCPA.get_right())
+    def test_KEM_INDCPA_definitions_equivalent(self): 
+        assert canonicalize_function(KEM.INDCPAv2.get_left()) == canonicalize_function(KEM.INDCPA.get_left())
+        assert canonicalize_function(KEM.INDCPAv2.get_right()) == canonicalize_function(KEM.INDCPA.get_right())
