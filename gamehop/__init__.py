d7e9e17ac681cc862e9581d21f1a5bcfdadf9bb8
Douglas Stebila <dstebila@uwaterloo.ca>
2021-08-16 10:29:58 -0400

    Remove unused code

--- gamehop/__init__.py
+++ gamehop/__init__.py
@@ -1,8 +1,1 @@
-__all__ = ['primitives']
-
-from typing import Any, Callable, List, Set, Union, cast
-from gamehop import inlining
-from gamehop import verification
-from gamehop.primitives import Crypto
-import ast
-import inspect
+__all__ = ['inlining', 'primitives', 'verification', 'lists', 'proofs', 'utils']
