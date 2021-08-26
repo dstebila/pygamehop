f23e3e0e2f83e779e39b80a6095b39055b186640
Douglas Stebila <dstebila@uwaterloo.ca>
2021-08-26 15:30:51 -0400

    Some updates to NestedPKE

--- examples/nestedPKE/nestedPKE.py
+++ examples/nestedPKE/nestedPKE.py
@@ -1,4 +1,4 @@
-from typing import NewType, Tuple, Union, Generic, Set
+from typing import cast, NewType, Tuple, Union, Generic, Set
 
 from gamehop.primitives import Crypto
 from gamehop.primitives.PKE import PKEScheme
@@ -15,19 +15,29 @@ class NestedPKE(PKEScheme):
         def __init__(self, sk1: PKE1.SecretKey, sk2: PKE2.SecretKey):
             self.sk1 = sk1
             self.sk2 = sk2
+    class Ciphertext(PKEScheme.Ciphertext): pass
+    class Message(PKEScheme.Message): pass
     @staticmethod
-    def KeyGen():
+    def KeyGen() -> Tuple[PublicKey, SecretKey]:
         (pk1, sk1) = PKE1.KeyGen()
         (pk2, sk2) = PKE2.KeyGen()
-        return (NestedPKE.PublicKey(pk1, pk2), NestedPKE.SecretKey(sk1, sk2))
+        pkprime = NestedPKE.PublicKey(pk1, pk2)
+        skprime = NestedPKE.SecretKey(sk1, sk2)
+        return (pkprime, skprime)
     @staticmethod
-    def Encrypt(pk, msg):
+    def Encrypt(pk: PublicKey, msg: Message) -> Ciphertext: # type: ignore[override]
         ct1 = PKE1.Encrypt(pk.pk1, msg)
-        ct2 = PKE2.Encrypt(pk.pk2, ct1)
-        return ct2
+        pt2 = cast(PKE2.Message, ct1)
+        ct2 = PKE2.Encrypt(pk.pk2, pt2)
+        ctprime = cast(NestedPKE.Ciphertext, ct2)
+        return ctprime
     @staticmethod
-    def Decrypt(sk, ct):
+    def Decrypt(sk: SecretKey, ct: Ciphertext) -> Union[Crypto.Reject, Message]: # type: ignore[override]
         pt2 = PKE2.Decrypt(sk.sk2, ct)
-        if pt2 == Crypto.Reject: r = Crypto.Reject
-        else: r = PKE1.Decrypt(sk.sk1, pt2)
+        if pt2 == Crypto.Reject:
+            r: Union[Crypto.Reject, NestedPKE.Message] = Crypto.Reject()
+        else:
+            ct1 = cast(PKE1.Ciphertext, pt2)
+            pt1 = PKE1.Decrypt(sk.sk1, ct1)
+            r = cast(NestedPKE.Message, pt1)
         return r
--- examples/nestedPKE/nestedPKE_is_INDCPA2.py
+++ examples/nestedPKE/nestedPKE_is_INDCPA2.py
@@ -1,7 +1,33 @@
+from typing import cast, Tuple, Type
+
 import gamehop.inlining
-from gamehop.primitives import PKE
+from gamehop.primitives import Crypto, PKE
+from gamehop.proofs2 import Proof
 
-from nestedPKE import NestedPKE
+from nestedPKE import NestedPKE, PKE1, PKE2
 
-s = gamehop.inlining.inline_scheme_into_game(NestedPKE, PKE.INDCPA_Left)
-print(s)
+# term we're trying to bound, first in terms of PKE1 security
+proof1 = Proof(NestedPKE, PKE.INDCPA)
+
+# game hop:
+# encrypt m1 rather than m0; simulate PKE2's encryption
+# proven by constructing reduction from distinguishing the previous game and this game to distinguishing PKE.INDCPA (with b = 0) from PKE.INDCPA (with b = 1) for PKE1
+class R1(PKE.INDCPA_Adversary, Crypto.Reduction): # this is a INDCPA adversary for PKE1
+    def __init__(self, Scheme: Type[PKE1], inner_adversary: PKE.INDCPA_Adversary):
+        self.Scheme = Scheme
+        self.inner_adversary = inner_adversary
+    def challenge(self, pk1: PKE1.PublicKey) -> Tuple[PKE1.Message, PKE1.Message]:
+        (self.pk2, self.sk2) = PKE2.KeyGen()
+        pk_double = NestedPKE.PublicKey(pk1, self.pk2)
+        (m0, m1) = self.inner_adversary.challenge(pk_double)
+        return (m0, m1)
+    def guess(self, ct1: PKE1.Ciphertext) -> Crypto.Bit:
+        pt2 = cast(PKE2.Message, ct1)
+        ct2 = PKE2.Encrypt(self.pk2, pt2)
+        ctprime = cast(NestedPKE.Ciphertext, ct2)
+        return self.inner_adversary.guess(ctprime)
+
+print(gamehop.inlining.inline_scheme_into_game(NestedPKE, PKE.INDCPA.get_left()))
+print(gamehop.inlining.inline_reduction_into_game(R1, PKE.INDCPA.get_left(), PKE1, PKE.INDCPA.get_left(), NestedPKE))
+
+# proof1.addDistinguishingProofStep(PKE.INDCPA, 'pke1', R1)
