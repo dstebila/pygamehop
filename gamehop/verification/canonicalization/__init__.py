a40521ecf5525867d82cc3f74441107d6733ae1c
Douglas Stebila <dstebila@uwaterloo.ca>
2021-08-30 16:03:12 -0400

    Simplify canonicalize_return

--- gamehop/verification/canonicalization/__init__.py
+++ gamehop/verification/canonicalization/__init__.py
@@ -16,31 +16,20 @@ def canonicalize_function_name(f: ast.FunctionDef, name = 'f') -> None:
 def canonicalize_return(f: ast.FunctionDef) -> None:
     """Modify (in place) the given function definition to simplify its return statement to be either a constant or a single variable."""
     class ReturnExpander(ast.NodeTransformer):
-        def __init__(self, vars, prefix = 'ret'):
-            self.vars = vars
-            self.prefix = prefix
         def visit_Return(self, node):
             # if it directly returns a constant or variable, consider that canonical
             if isinstance(node.value, ast.Constant): return node
             if isinstance(node.value, ast.Name): return node
             # otherwise, make a new assignment for that return value and then return the newly assigned variable
-            # find a unique name for the return value
-            i = 0
-            retname = '{:s}{:d}'.format(self.prefix, i)
-            while retname in self.vars:
-                i += 1
-                retname = '{:s}{:d}'.format(self.prefix, i)
-            self.vars.append(retname)
             assign = ast.Assign(
-                targets = [ast.Name(id=retname, ctx=ast.Store())],
+                targets = [ast.Name(id='ρ', ctx=ast.Store())],
                 value = node.value
             )
             ret = ast.Return(
-                value = ast.Name(id=retname, ctx=ast.Load())
+                value = ast.Name(id='ρ', ctx=ast.Load())
             )
             return [assign, ret]
-    vars = internal.find_all_variables(f)
-    fprime = ReturnExpander(vars).visit(f)
+    fprime = ReturnExpander().visit(f)
     f.body = fprime.body
     ast.fix_missing_locations(f)
 
--- tests/gamehop/verification/canonicalization/test_return.py
+++ tests/gamehop/verification/canonicalization/test_return.py
@@ -5,58 +5,19 @@ import unittest
 import gamehop.inlining.internal
 import gamehop.verification.canonicalization
 
-def f_constant(x, y):
-    a = 7
-    return 3
-def f_constant_expected_result(x, y):
-    a = 7
-    return 3
-def f_variable(x, y):
-    a = 7
-    return a
-def f_variable_expected_result(x, y):
-    a = 7
-    return a
-def f_tuple(x, y):
-    a = 7
-    return (a, x, y)
-def f_tuple_expected_result(x, y):
-    a = 7
-    ret0 = (a, x, y)
-    return ret0
-def f_attribute(x, y):
-    return x.a
-def f_attribute_expected_result(x, y):
-    ret0 = x.a
-    return ret0
-def f_operator(x, y):
-    a = 7
-    return y + b
-def f_operator_expected_result(x, y):
-    a = 7
-    ret0 = y + b
-    return ret0
-def f_multiple_return(x, y):
-    if x:
-        return y + 1
-    else:
-        return y + 2
-def f_multiple_return_expected_result(x, y):
-    if x:
-        ret0 = y + 1
-        return ret0
-    else:
-        ret1 = y + 2
-        return ret1
-
-
 def expected_result(f):
-    s = inspect.getsource(f)
-    s = s.replace('_expected_result', '')
-    return ast.unparse(ast.parse(s))
+    fdef = gamehop.utils.get_function_def(f)
+    fdef.name = fdef.name.replace('_expected_result', '')
+    return ast.unparse(fdef)
 
 class TestCanonicalizeReturn(unittest.TestCase):
     def test_constant(self):
+        def f_constant(x, y):
+            a = 7
+            return 3
+        def f_constant_expected_result(x, y):
+            a = 7
+            return 3
         f = gamehop.utils.get_function_def(f_constant)
         gamehop.verification.canonicalization.canonicalize_return(f)
         self.assertEqual(
@@ -64,13 +25,38 @@ class TestCanonicalizeReturn(unittest.TestCase):
             expected_result(f_constant_expected_result)
         )
     def test_variable(self):
+        def f_variable(x, y):
+            a = 7
+            return a
+        def f_variable_expected_result(x, y):
+            a = 7
+            return a
         f = gamehop.utils.get_function_def(f_variable)
         gamehop.verification.canonicalization.canonicalize_return(f)
         self.assertEqual(
             ast.unparse(f),
             expected_result(f_variable_expected_result)
         )
+    def test_tuple(self):
+        def f_tuple(x, y):
+            a = 7
+            return (a, x, y)
+        def f_tuple_expected_result(x, y):
+            a = 7
+            ρ = (a, x, y)
+            return ρ
+        f = gamehop.utils.get_function_def(f_tuple)
+        gamehop.verification.canonicalization.canonicalize_return(f)
+        self.assertEqual(
+            ast.unparse(f),
+            expected_result(f_tuple_expected_result)
+        )
     def test_attribute(self):
+        def f_attribute(x, y):
+            return x.a
+        def f_attribute_expected_result(x, y):
+            ρ = x.a
+            return ρ
         f = gamehop.utils.get_function_def(f_attribute)
         gamehop.verification.canonicalization.canonicalize_return(f)
         self.assertEqual(
@@ -78,16 +64,16 @@ class TestCanonicalizeReturn(unittest.TestCase):
             expected_result(f_attribute_expected_result)
         )
     def test_operator(self):
+        def f_operator(x, y):
+            a = 7
+            return y + a
+        def f_operator_expected_result(x, y):
+            a = 7
+            ρ = y + a
+            return ρ
         f = gamehop.utils.get_function_def(f_operator)
         gamehop.verification.canonicalization.canonicalize_return(f)
         self.assertEqual(
             ast.unparse(f),
             expected_result(f_operator_expected_result)
-        )
-    def test_multiple_return(self):
-        f = gamehop.utils.get_function_def(f_multiple_return)
-        gamehop.verification.canonicalization.canonicalize_return(f)
-        self.assertEqual(
-            ast.unparse(f),
-            expected_result(f_multiple_return_expected_result)
         )
