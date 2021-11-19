8dbec4693afa6ebe466b02f9910dd992016507c9
Douglas Stebila <dstebila@uwaterloo.ca>
2021-11-19 17:25:09 -0500

    Update test cases for TypeVars

--- tests/gamehop/inlining/test_inline_reduction_into_game.py
+++ tests/gamehop/inlining/test_inline_reduction_into_game.py
@@ -2,7 +2,7 @@ import ast
 import random
 import unittest
 
-from typing import cast, Type
+from typing import Generic, Type, TypeVar
 
 import gamehop.inlining
 from gamehop.primitives import Crypto
@@ -21,17 +21,18 @@ def expected_result(g):
 # Then we have inline R into G1 which we do by inline_reduction_into_game(R, G1, G2) and check that against the same expected result from the previous step.
 # Well, these two different inlinings won't actually be equal as strings yet because we haven't applied canonicalization, so we'll list the two different versions we expect to get, which I have inspected and believe are logically equivalent.
 
-class P1(Crypto.Scheme):
-    class PublicKey(): pass
-    class Ciphertext(): pass
+PublicKey1 = TypeVar('PublicKey1')
+Ciphertext1 = TypeVar('Ciphertext1')
+
+class P1(Crypto.Scheme, Generic[PublicKey1, Ciphertext1]):
     @staticmethod
-    def KeyGen() -> PublicKey: pass
+    def KeyGen() -> PublicKey1: pass
     @staticmethod
-    def Encrypt(pk: PublicKey, msg: str) -> Ciphertext: pass
-class G1_Adversary(Crypto.Adversary): # game idea: is ct the encryption of "hi" or "bye"?
-    def hi_or_bye(self, pk: P1.PublicKey, ct: P1.Ciphertext) -> int: pass
-class G1(Crypto.Game):
-    def __init__(self, Scheme: Type[P1], Adversary: Type[G1_Adversary]):
+    def Encrypt(pk: PublicKey1, msg: str) -> Ciphertext1: pass
+class G1_Adversary(Crypto.Adversary, Generic[PublicKey1, Ciphertext1]): # game idea: is ct the encryption of "hi" or "bye"?
+    def hi_or_bye(self, pk: PublicKey1, ct: Ciphertext1) -> int: pass
+class G1(Crypto.Game, Generic[PublicKey1, Ciphertext1]):
+    def __init__(self, Scheme: Type[P1[PublicKey1, Ciphertext1]], Adversary: Type[G1_Adversary[PublicKey1, Ciphertext1]]):
         self.Scheme = Scheme
         self.adversary = Adversary(Scheme)
     def main(self) -> Crypto.Bit:
@@ -42,17 +43,19 @@ class G1(Crypto.Game):
         bstar = self.adversary.hi_or_bye(pk, ct)
         ret = 1 if b == bstar else 0
         return Crypto.Bit(ret)
-class P2(Crypto.Scheme):
-    class PK(): pass
-    class CT(): pass
+
+PublicKey2 = TypeVar('PublicKey2')
+Ciphertext2 = TypeVar('Ciphertext2')
+
+class P2(Crypto.Scheme, Generic[PublicKey2, Ciphertext2]):
     @staticmethod
-    def KG() -> PK: pass
+    def KG() -> PublicKey2: pass
     @staticmethod
-    def ENC(pk: PK, msg: str) -> CT: pass
-class G2_Adversary(Crypto.Adversary): # game idea: is ct the encryption of "hi!" or not?"
-    def hi_or_not(self, pk: P2.PK, ct: P2.CT) -> bool: pass
-class G2(Crypto.Game):
-    def __init__(self, Scheme: Type[P2], Adversary: Type[G2_Adversary]):
+    def ENC(pk: PublicKey2, msg: str) -> Ciphertext2: pass
+class G2_Adversary(Crypto.Adversary, Generic[PublicKey2, Ciphertext2]): # game idea: is ct the encryption of "hi!" or not?"
+    def hi_or_not(self, pk: PublicKey2, ct: Ciphertext2) -> bool: pass
+class G2(Crypto.Game, Generic[PublicKey2, Ciphertext2]):
+    def __init__(self, Scheme: Type[P2[PublicKey2, Ciphertext2]], Adversary: Type[G2_Adversary[PublicKey2, Ciphertext2]]):
         self.Scheme = Scheme
         self.adversary = Adversary(Scheme)
     def main(self) -> Crypto.Bit:
@@ -64,29 +67,25 @@ class G2(Crypto.Game):
         bstar = 0 if bnew else 1
         ret = 1 if b == bstar else 0
         return Crypto.Bit(ret)
-class P2fromP1(P2):
-    class PK(P2.PK):
-        def __init__(self, pk: P1.PublicKey):
-            self.pk = pk
-    class CT(P2.CT):
-        def __init__(self, ct: P1.Ciphertext):
-            self.ct = ct
+
+PK = TypeVar('PK')
+CT = TypeVar('CT')
+
+P1Instance = P1[PK, CT]
+
+class P2fromP1(P2[PK, CT]):
     @staticmethod
     def KG():
-        pk = P1.KeyGen()
-        return P2fromP1.PK(pk)
+        return P1Instance.KeyGen()
     @staticmethod
     def ENC(pk, msg):
-        ct = P1.Encrypt(pk.pk, msg)
-        return P2fromP1.CT(ct)
+        return P1Instance.Encrypt(pk, msg)
 class R(Crypto.Reduction, G1_Adversary):
-    def __init__(self, Scheme: Type[P1], inner_adversary: G2_Adversary):
+    def __init__(self, Scheme: Type[P1Instance], inner_adversary: G2_Adversary[PK, CT]):
         self.Scheme = Scheme
         self.inner_adversary = inner_adversary
-    def hi_or_bye(self, pk: P1.PublicKey, ct: P1.Ciphertext) -> int:
-        pkprime = P2fromP1.PK(pk)
-        ctprime = P2fromP1.CT(ct)
-        g = self.inner_adversary.hi_or_not(pkprime, ctprime)
+    def hi_or_bye(self, pk: PK, ct: CT) -> int:
+        g = self.inner_adversary.hi_or_not(pk, ct)
         ret = 0 if g else 1
         return ret
 
@@ -94,19 +93,15 @@ class TestInlineReductionIntoGame(unittest.TestCase):
 
     def test_P2fromP1_into_G2(self):
         self.maxDiff = None
-        class G2_expected_result(Crypto.Game):
-            def __init__(self, Adversary: Type[G2_Adversary]):
+        class G2_expected_result(Crypto.Game, Generic[PK, CT]):
+            def __init__(self, Adversary: Type[G2_Adversary[PK, CT]]):
                 self.Scheme = P2fromP1
                 self.adversary = Adversary(P2fromP1)
             def main(self) -> Crypto.Bit:
-                P2fromP1_KGᴠ1ⴰpk = P1.KeyGen()
-                pk = P2fromP1.PK.__new__(P2fromP1.PK)
-                pk.pk = P2fromP1_KGᴠ1ⴰpk
+                pk = P1Instance.KeyGen()
                 b = random.choice([0, 1])
                 msge = "hi!" if b == 0 else "bye"
-                P2fromP1_ENCᴠ1ⴰct = P1.Encrypt(pk.pk, msge)
-                ct = P2fromP1.CT.__new__(P2fromP1.CT)
-                ct.ct = P2fromP1_ENCᴠ1ⴰct
+                ct = P1Instance.Encrypt(pk, msge)
                 bnew = self.adversary.hi_or_not(pk, ct)
                 bstar = 0 if bnew else 1
                 ret = 1 if b == bstar else 0
@@ -117,24 +112,20 @@ class TestInlineReductionIntoGame(unittest.TestCase):
 
     def test_R_into_G1(self):
         self.maxDiff = None
-        class G2_expected_result(Crypto.Game):
-            def __init__(self, Adversary: Type[G2_Adversary]):
+        class G2_expected_result(Crypto.Game, Generic[PK, CT]):
+            def __init__(self, Adversary: Type[G2_Adversary[PK, CT]]):
                 self.Scheme = P2fromP1
                 self.adversary = Adversary(P2fromP1)
             def main(self) -> Crypto.Bit:
-                pk = P1.KeyGen()
+                pk = P1Instance.KeyGen()
                 b = random.choice([0, 1])
                 msge = 'hi!' if b == 0 else 'bye'
-                ct = P1.Encrypt(pk, msge)
-                R_hi_or_byeᴠ1ⴰpkprime = P2fromP1.PK.__new__(P2fromP1.PK)
-                R_hi_or_byeᴠ1ⴰpkprime.pk = pk
-                R_hi_or_byeᴠ1ⴰctprime = P2fromP1.CT.__new__(P2fromP1.CT)
-                R_hi_or_byeᴠ1ⴰctprime.ct = ct
-                R_hi_or_byeᴠ1ⴰg = self.adversary.hi_or_not(R_hi_or_byeᴠ1ⴰpkprime, R_hi_or_byeᴠ1ⴰctprime)
+                ct = P1Instance.Encrypt(pk, msge)
+                R_hi_or_byeᴠ1ⴰg = self.adversary.hi_or_not(pk, ct)
                 R_hi_or_byeᴠ1ⴰret = 0 if R_hi_or_byeᴠ1ⴰg else 1
                 bstar = R_hi_or_byeᴠ1ⴰret
                 ret = 1 if b == bstar else 0
                 return Crypto.Bit(ret)
         self.assertEqual(
-            gamehop.inlining.inline_reduction_into_game(R, G1, P1, "P1", G2, P2fromP1, G2_Adversary),
+            gamehop.inlining.inline_reduction_into_game(R, G1, P1Instance, "P1Instance", G2, P2fromP1, G2_Adversary),
             expected_result(G2_expected_result))
