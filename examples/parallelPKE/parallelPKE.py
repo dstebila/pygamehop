b7bd639431c28b4928527cd524809eeb54bbe92d
Douglas Stebila <dstebila@uwaterloo.ca>
2021-11-23 11:14:37 -0500

    Update parallelPKE to use typevars

--- examples/parallelPKE/parallelPKE.py
+++ examples/parallelPKE/parallelPKE.py
@@ -1,45 +1,41 @@
-from typing import cast, Tuple, Union
+from typing import cast, Generic, Optional, Sized, Tuple, TypeVar
 
 from gamehop.primitives import Crypto
 from gamehop.primitives.PKE import PKEScheme
 
-class PKE1(PKEScheme): pass
-class PKE2(PKEScheme): pass
+PK1 = TypeVar('PK1')
+SK1 = TypeVar('SK1')
+CT1 = TypeVar('CT1')
+PK2 = TypeVar('PK2')
+SK2 = TypeVar('SK2')
+CT2 = TypeVar('CT2')
+PT12 = TypeVar('PT12', bound=Sized) # Use the same plaintext space for both schemes
 
-class ParallelPKE(PKEScheme):
-    class PublicKey(PKEScheme.PublicKey):
-        def __init__(self, pk1: PKE1.PublicKey, pk2: PKE2.PublicKey):
-            self.pk1 = pk1
-            self.pk2 = pk2
-    class SecretKey(PKEScheme.SecretKey):
-        def __init__(self, sk1: PKE1.SecretKey, sk2: PKE2.SecretKey):
-            self.sk1 = sk1
-            self.sk2 = sk2
-    class Ciphertext(PKEScheme.Ciphertext):
-        def __init__(self, ct1: PKE1.Ciphertext, ct2: PKE2.Ciphertext):
-            self.ct1 = ct1
-            self.ct2 = ct2
-    class Message(PKEScheme.Message): pass
+PKE1 = PKEScheme[PK1, SK1, CT1, PT12]
+PKE2 = PKEScheme[PK2, SK2, CT2, PT12]
 
+class ParallelPKE(
+    Generic[PK1, PK2, SK1, SK2, CT1, CT2, PT12],
+    PKEScheme[Tuple[PK1, PK2], Tuple[SK1, SK2], Tuple[CT1, CT2], PT12]):
     @staticmethod
-    def KeyGen() -> Tuple[PublicKey, SecretKey]:
+    def KeyGen():
         (pk1, sk1) = PKE1.KeyGen()
         (pk2, sk2) = PKE2.KeyGen()
-        pkprime = ParallelPKE.PublicKey(pk1, pk2)
-        skprime = ParallelPKE.SecretKey(sk1, sk2)
-        return (pkprime, skprime)
+        return ((pk1, pk2), (sk1, sk2))
     @staticmethod
-    def Encrypt(pk: PublicKey, msg: Message) -> Ciphertext: # type: ignore[override]
-        ct1 = PKE1.Encrypt(pk.pk1, msg)
-        ct2 = PKE2.Encrypt(pk.pk2, msg)
-        ct = ParallelPKE.Ciphertext(ct1, ct2)
-        return ct
+    def Encrypt(npk, msg):
+        (pk1, pk2) = npk
+        ct1 = PKE1.Encrypt(pk1, msg)
+        ct2 = PKE2.Encrypt(pk2, msg)
+        return (ct1, ct2)
     @staticmethod
-    def Decrypt(sk: SecretKey, ct: Ciphertext) -> Union[Crypto.Reject, Message]: # type: ignore[override]
-        pt1 = PKE1.Decrypt(sk.sk1, ct.ct1)
-        pt2 = PKE2.Decrypt(sk.sk2, ct.ct2)
-        if pt1 == Crypto.Reject or pt1 == Crypto.Reject or pt1 != pt2:
-            r: Union[Crypto.Reject, ParallelPKE.Message] = Crypto.Reject()
+    def Decrypt(nsk, nct):
+        (sk1, sk2) = nsk
+        (ct1, ct2) = nct
+        pt1 = PKE1.Decrypt(sk1, ct1)
+        pt2 = PKE2.Decrypt(sk2, ct2)
+        if pt1 is None or pt1 is None or pt1 != pt2:
+            r: Optional[PT12] = None
         else:
-            r = cast(ParallelPKE.Message, pt1)
+            r = pt1
         return r
