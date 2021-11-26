6ca5f125f69d870d2454a19e2a6ce648a7a13b08
Douglas Stebila <dstebila@uwaterloo.ca>
2021-11-26 10:33:52 -0500

    Add simplified rewriting steps with string substitution

--- examples/KEMfromPKE/KEMfromPKE_is_INDCPA.py
+++ examples/KEMfromPKE/KEMfromPKE_is_INDCPA.py
@@ -17,46 +17,9 @@ proof = Proof(KEMfromPKE, KEM.INDCPA)
 # This will be done by a rewriting step.
 # The rewriting step also renames one member variable to a local variable since the
 # canonicalization engine can't handle that properly yet.
-
-class Rewrite0_Left(Crypto.Game, Generic[PK, SK, CT, SS]):
-
-    def __init__(v0, v1: Type[KEM.INDCPA_Adversary[PK, SK, CT, SS]]):
-        v0.Scheme = KEMfromPKE
-        v0.adversary = v1(KEMfromPKE)
-
-    def main(v0) -> Crypto.Bit:
-        v1 = KEMfromPKE.uniformSharedSecret()
-        (v2, v3) = InnerPKE.KeyGen()
-        v0ss0 = KEMfromPKE.uniformSharedSecret()
-        v4 = len(v0ss0)
-        v5 = len(v1)
-        v6 = InnerPKE.Encrypt(v2, v0ss0)
-        v7 = True
-        v8 = v0.adversary.guess(v2, v6, v0ss0)
-        v9 = Crypto.Bit(0)
-        v10 = v8 if v7 else v9
-        return v10
-
-class Rewrite0_Right(Crypto.Game, Generic[PK, SK, CT, SS]):
-
-    def __init__(v0, v1: Type[KEM.INDCPA_Adversary[PK, SK, CT, SS]]):
-        v0.Scheme = KEMfromPKE
-        v0.adversary = v1(KEMfromPKE)
-
-    def main(v0) -> Crypto.Bit:
-        v1 = KEMfromPKE.uniformSharedSecret()
-        (v2, v3) = InnerPKE.KeyGen()
-        v0.ss0 = KEMfromPKE.uniformSharedSecret()
-        v4 = len(v0.ss0)
-        v5 = len(v1)
-        v6 = InnerPKE.Encrypt(v2, v0.ss0)
-        v7 = v4 == v5
-        v8 = v0.adversary.guess(v2, v6, v0.ss0)
-        v9 = Crypto.Bit(0)
-        v10 = v8 if v7 else v9
-        return v10
-
-proof.add_rewriting_proof_step(Rewrite0_Left, Rewrite0_Right)
+# This rewriting step will be phrased by rewriting the next game to get the previous 
+# game, so we have to defer inserting this rewriting step until after the next step 
+# has been added.
 
 # Game 2 sends the encryption of an independent random value instead of the actual shared secret.
 
@@ -85,53 +48,23 @@ class R1(Crypto.Reduction,
 
 proof.add_distinguishing_proof_step(R1, PKE.INDCPA, InnerPKE, "InnerPKE")
 
+proof.insert_simple_rewriting_proof_step_before({
+    "self.ss0": "selfss0",
+    "if len(m0) == len(m1)": "if True"
+})
+
 # Need to again codify an implicit assumption in KEMfromPKE that encryptions of 
 # equal-length messages yield equal-length ciphertexts.
 # This will be done by a rewriting step.
 # The rewriting step also renames one member variable to a local variable since the
 # canonicalization engine can't handle that properly yet.
 
-class Rewrite2_Left(Crypto.Game, Generic[PK, SK, CT, SS]):
+proof.insert_simple_rewriting_proof_step_after({
+    "self.ss0": "selfss0",
+    "if len(m0) == len(m1)": "if True"
+})
 
-    def __init__(v0, v1: Type[KEM.INDCPA_Adversary[PK, SK, CT, SS]]):
-        v0.Scheme = KEMfromPKE
-        v0.adversary = v1(KEMfromPKE)
-
-    def main(v0) -> Crypto.Bit:
-        (v1, v2) = InnerPKE.KeyGen()
-        v3 = KEMfromPKE.uniformSharedSecret()
-        v0.ss0 = KEMfromPKE.uniformSharedSecret()
-        v4 = len(v0.ss0)
-        v5 = len(v3)
-        v6 = InnerPKE.Encrypt(v1, v3)
-        v7 = v4 == v5
-        v8 = v0.adversary.guess(v1, v6, v0.ss0)
-        v9 = Crypto.Bit(0)
-        v10 = v8 if v7 else v9
-        return v10
-
-class Rewrite2_Right(Crypto.Game, Generic[PK, SK, CT, SS]):
-
-    def __init__(v0, v1: Type[KEM.INDCPA_Adversary[PK, SK, CT, SS]]):
-        v0.Scheme = KEMfromPKE
-        v0.adversary = v1(KEMfromPKE)
-
-    def main(v0) -> Crypto.Bit:
-        (v1, v2) = InnerPKE.KeyGen()
-        v3 = KEMfromPKE.uniformSharedSecret()
-        v0ss0 = KEMfromPKE.uniformSharedSecret()
-        v4 = len(v0ss0)
-        v5 = len(v3)
-        v6 = InnerPKE.Encrypt(v1, v3)
-        v7 = True
-        v8 = v0.adversary.guess(v1, v6, v0ss0)
-        v9 = Crypto.Bit(0)
-        v10 = v8 if v7 else v9
-        return v10
-
-proof.add_rewriting_proof_step(Rewrite2_Left, Rewrite2_Right)
-
-assert proof.check(print_hops=False, print_canonicalizations=False, print_diffs=False, abort_on_failure=False)
+assert proof.check(print_hops=True, print_canonicalizations=True, print_diffs=True, abort_on_failure=False)
 print("Theorem:")
 print(proof.advantage_bound())
 
